a1fb9a4659285f85478af3e740073d4a17a5a758
Pour Tokens Developer <pourtokens@proton.me>
2024-10-21 20:03:21 +0100

    Refactor: Add ref forwarding to Bundles and integrate tokenRef in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import Header from "./components/header/Header";
 import Bundles from "./components/home/Bundles";
 import Offers from "./components/home/Offers";
 import CTA from "./components/home/CTA";
+import { useRef } from "react";
 
 const App = () => {
+	const tokenRef = useRef<HTMLDivElement>(null);
+
 	return (
 		<div className="flex-grow">
-			<Header />
+			<Header tokenRef={tokenRef} />
 			<Offers />
-			<Bundles />
-			<CTA />
+			<Bundles ref={tokenRef} />
+			<CTA tokenRef={tokenRef} />
 		</div>
 	);
 };
