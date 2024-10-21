daf1c9af5f46a15ec3b0d696424222bfc8c2189f
Pour Tokens Developer <pourtokens@proton.me>
2024-10-21 16:23:30 +0100

    fix(scrolling): Fix scrolling bug

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import App from "./App";
 import TokenPage from "./components/token/TokenPage";
 import Footer from "./components/footer/Footer";
 import NotFoundPage from "./components/not-found/NotFoundPage";
+import { ScrollToTop } from "./lib/utils";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -19,6 +20,7 @@ root.render(
 	<React.StrictMode>
 		<div className="min-h-dvh flex flex-col justify-between bg-gradient-to-b from-cyan-900 to-blue-900 text-white">
 			<Router>
+				<ScrollToTop />
 				<GAnalytics />
 				<Routes>
 					<Route path="/" element={<App />} />
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function ScrollToTop() {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
 }
